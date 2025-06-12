e2ff2d8fa6b406b0eb06d0caa4fc953bd9c0b402
김정민 <raon37910@gmail.com>
2025-06-12 17:23:49 +0900

    feat: [LiveBoard] 경기 일정 UI 추가

--- src/components/live-board/DatePicker.vue
+++ src/components/live-board/DatePicker.vue
@@ -69,7 +69,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .date-picker {
   display: flex;
   align-items: center;
--- src/page/mobile/live-board/Main.vue
+++ src/page/mobile/live-board/Main.vue
@@ -1,15 +1,26 @@
 <template>
-  <DatePicker />
-  라이브 보드 페이지
+  <div class="live-board-content">
+    <DatePicker/>
+    <MatchList/>
+  </div>
 </template>
+
+<style scoped>
+
+.live-board-content {
+  height: 100%;
+}
+</style>
 
 <script>
 import DatePicker from '@/components/live-board/DatePicker.vue'
+import MatchList from "@/components/live-board/MatchList.vue";
 
 export default {
   name: 'LiveBoardMain',
   components: {
-    DatePicker
+    DatePicker,
+    MatchList,
   }
 }
 </script>
