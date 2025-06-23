8871ce0867d8bd47e8b45593c1e4d1e2b008f4f8
Juungmini0601 <jungmini0601@gmail.com>
2025-06-23 14:59:17 +0900

    [채팅] 라이브보드 / 교환 채팅 기능 구현 (#4)
    * 라이브보드 채팅 구현 임시 커밋
    
    * 라이브보드 현재 채팅 받아서 익명으로 하는거 까지 구현됨 로그인 붙여야 함
    
    * 라이브 보드 채팅 구현
    
    * chore: merge
    
    * feat: 교환 1대1 채팅 구현

--- src/router/router.js
+++ src/router/router.js
@@ -13,144 +13,151 @@ import Login from "@/page/mobile/user/Login.vue";
 import OAuth2Redirect from "@/page/mobile/util/OAuth2Redirect.vue";
 import MyPage from "@/page/mobile/user/MyPage.vue";
 import Channel from "@/page/mobile/user/Channel.vue";
+import Chatroom from "@/page/mobile/exchange/Chatroom.vue";
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/liveboard/match'
-    },
-    {
-        path: '/liveboard',
-        component: MainPage,
-        children: [
-            {
-                path: 'match',
-                name: 'liveboard-match',
-                component: LiveBoardMatch,
-                meta: { title: '라이브보드' }
-            },
-            {
-                path: 'match/room/:roomId',
-                name: 'liveboard-room',
-                component: LiveBoardRoom,
-                meta: { title: '라이브보드' }
-            }
-        ]
-    },
-    {
-        path: '/record',
-        component: MainPage,
-        children: [
-            {
-                path: '',
-                name: 'record-main',
-                component: recordMain,
-                meta: { title: '기록' }
-            }
-        ]
-    },
-    {
-        path: '/exchange',
-        component: MainPage,
-        children: [
-            {
-                path: '',
-                name: 'exchange-main',
-                component: exchangeMain,
-                meta: { title: '교환' }
-            }
-        ]
-    },
-    {
-        path: '/alarm',
-        component: MainPage,
-        children: [
-            {
-                path: '',
-                name: 'alarm-main',
-                component: alarmMain,
-                meta: { title: '알람' }
-            },
-            {
-                path: 'teams',
-                name: 'Teams',
-                component: Teams,
-                meta: { requiresAuth: true, title: '알람' }
-            },
-            {
-                path: 'teams/:teamId/schedule',
-                name: 'TeamSchedule',
-                component: Schedule,
-                meta: { requiresAuth: true, title: '알람' }
-            },
-            {
-                path: 'teams/:teamId/reservation/:matchId',
-                name: 'MatchReservation',
-                component: MatchReservation,
-                meta: { requiresAuth: true, title: '알람' }
-            }
-        ]
-    },
-    {
-        path: '/stadiuminfo',
-        component: MainPage,
-        children: [
-            {
-                path: '',
-                name: 'stadium-main',
-                component: stadiumInfoMain,
-                meta: { title: '경기장' }
-            }
-        ]
-    },
-    {
-        path: '/login',
-        component: MainPage,
-        children: [
-            {
-                path: '',
-                name: 'login',
-                component: Login,
-                meta: { title: '로그인' }
-            }
-        ]
-    },
-    {
-        path: '/mypage',
-        component: MainPage,
-        children: [
-            {
-                path: '',
-                name: 'mypage',
-                component: MyPage,
-                meta: { requiresAuth: true, title: '마이페이지' }
-            }
-        ]
-    },
-    {
-        path: '/oauth2/redirect',
-        name: 'OauthRedirect',
-        component: OAuth2Redirect
-    },
-    {
-        path: '/channel',
-        name: 'Channel',
-        component: Channel
-    }
+  {
+    path: "/",
+    redirect: "/liveboard/match",
+  },
+  {
+    path: "/liveboard",
+    component: MainPage,
+    children: [
+      {
+        path: "match",
+        name: "liveboard-match",
+        component: LiveBoardMatch,
+        meta: { title: "라이브보드" },
+      },
+      {
+        path: "match/room/:roomId",
+        name: "liveboard-room",
+        component: LiveBoardRoom,
+        meta: { title: "라이브보드" },
+      },
+    ],
+  },
+  {
+    path: "/record",
+    component: MainPage,
+    children: [
+      {
+        path: "",
+        name: "record-main",
+        component: recordMain,
+        meta: { title: "기록" },
+      },
+    ],
+  },
+  {
+    path: "/exchange",
+    component: MainPage,
+    children: [
+      {
+        path: "",
+        name: "exchange-main",
+        component: exchangeMain,
+        meta: { title: "교환" },
+      },
+      {
+        path: "chat/:roomId",
+        name: "chat-room",
+        component: Chatroom,
+        meta: { title: "교환 1대1 채팅" },
+      },
+    ],
+  },
+  {
+    path: "/alarm",
+    component: MainPage,
+    children: [
+      {
+        path: "",
+        name: "alarm-main",
+        component: alarmMain,
+        meta: { title: "알람" },
+      },
+      {
+        path: "teams",
+        name: "Teams",
+        component: Teams,
+        meta: { requiresAuth: true, title: "알람" },
+      },
+      {
+        path: "teams/:teamId/schedule",
+        name: "TeamSchedule",
+        component: Schedule,
+        meta: { requiresAuth: true, title: "알람" },
+      },
+      {
+        path: "teams/:teamId/reservation/:matchId",
+        name: "MatchReservation",
+        component: MatchReservation,
+        meta: { requiresAuth: true, title: "알람" },
+      },
+    ],
+  },
+  {
+    path: "/stadiuminfo",
+    component: MainPage,
+    children: [
+      {
+        path: "",
+        name: "stadium-main",
+        component: stadiumInfoMain,
+        meta: { title: "경기장" },
+      },
+    ],
+  },
+  {
+    path: "/login",
+    component: MainPage,
+    children: [
+      {
+        path: "",
+        name: "login",
+        component: Login,
+        meta: { title: "로그인" },
+      },
+    ],
+  },
+  {
+    path: "/mypage",
+    component: MainPage,
+    children: [
+      {
+        path: "",
+        name: "mypage",
+        component: MyPage,
+        meta: { requiresAuth: true, title: "마이페이지" },
+      },
+    ],
+  },
+  {
+    path: "/oauth2/redirect",
+    name: "OauthRedirect",
+    component: OAuth2Redirect,
+  },
+  {
+    path: "/channel",
+    name: "Channel",
+    component: Channel,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
-    const isLoggedIn = !!localStorage.getItem('jwt_token');
-    if (to.meta.requiresAuth && !isLoggedIn) {
-        next('/login');
-    } else {
-        next();
-    }
+  const isLoggedIn = !!localStorage.getItem("jwt_token");
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
